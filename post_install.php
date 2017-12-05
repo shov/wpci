c505ddd72fa4c37d156f437b739f6194dc99e1f5
Alexandr Shevchenko <ls.shov@gmail.com>
2017-12-05 14:23:15 +0300

    Fixed post install, tested how entry point works

--- post_install.php
+++ post_install.php
@@ -9,21 +9,23 @@ namespace PostInstallScript;
 define('BASE_PATH', __DIR__);
 define('WP_PATH', BASE_PATH . '/wordpress');
 
-if(!is_dir(WP_PATH)) {
-    throw new \Exception("Have no installrd wordpress!");
+if (!is_dir(WP_PATH)) {
+    throw new \Exception("Have no installed wordpress!");
 }
 
-$themeFolder = WP_PATH . '/themes/wpci';
-if(!is_dir($themeFolder)) {
-    if(is_file($themeFolder)) {
+$themeFolder = WP_PATH . '/wp-content/themes/wpci';
+if (!is_dir($themeFolder)) {
+    if (is_file($themeFolder)) {
         @unlink($themeFolder);
     }
     mkdir($themeFolder);
 }
 
-$functionsPhpPath = $themeFolder . '/functions.php';
-
-$functionsPhpContent = <<<'END'
+/**
+ * index.php, to start Wpci Application
+ */
+$indexPhpPath = $themeFolder . '/index.php';
+$indexPhpContent = <<<'END'
 <?php
 /**
  * Load wpci app
@@ -33,4 +35,31 @@ $app = require(ABSPATH . '/../app/App.php');
 $app->run();
 END;
 
-file_put_contents($functionsPhpPath, $functionsPhpContent);
+file_put_contents($indexPhpPath, $indexPhpContent);
+
+/**
+ * Just stubs to guarantee WP correct work
+ */
+$styleSheetsStubPath = $themeFolder . '/style.css';
+$styleSheetsStubContent = <<<'END'
+/*
+Theme Name: WPCI
+Theme URI: https://github.com/shov/wpci
+Author: Alexandr Shevchenko [Shov] ls.shov@gmail.com
+Author URI: https://github.com/shov/
+*/
+
+END;
+
+file_put_contents($styleSheetsStubPath, $styleSheetsStubContent);
+
+$screenshotPath = $themeFolder . '/screenshot.png';
+
+$screenshotImg = imagecreatetruecolor(300, 300);
+$bgColor = imagecolorallocate($screenshotImg, 255, 255, 255);
+$textColor = imagecolorallocate($screenshotImg, 0, 0, 0);
+
+imagefilledrectangle($screenshotImg, 0, 0, 300, 300, $bgColor);
+imagestring($screenshotImg, 5, 100, 100, "WPCI", $textColor);
+
+imagepng($screenshotImg, $screenshotPath);
