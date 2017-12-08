7da9288e13baf4ae0b635f9646aaba1963300235
Alexandr Shevchenko <ls.shov@gmail.com>
2017-12-08 18:24:29 +0300

    Clean

--- src/app/templates/hello.php
+++
@@ -1,5 +0,0 @@
-<?=$data['wp-head']?>
-<h1><?=$title?></h1>
-<br>
-<h1><?=$sub?></h1>
-<?=$data['wp-footer']?>
--- src/app/templates/index.php
+++
@@ -1,8 +0,0 @@
-<?=$data['wp-head']?>
-<div>
-    <h1><?=$title?></h1>
-    <div class="b-content">
-        <p><?=$content?></p>
-    </div>
-</div>
-<?=$data['wp-footer']?>
