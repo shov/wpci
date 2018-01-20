96464afd66f9ed24b1c2ecc84ea950e5c264956d
Alexandr Shevchenko <shov@shov.pro>
2018-01-20 14:50:48 +0300

    Set test environment (#3)
    * Set test environment

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
